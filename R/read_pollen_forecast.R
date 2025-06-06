69d9f5afb959bd49a9dbc0680e00feb268cdce95
Alessia Bernacchia <alessia.bernacchia@gmail.com>
2025-06-06 12:17:54 +0200

    added example desc

--- R/read_pollen_forecast.R
+++ R/read_pollen_forecast.R
@@ -3,6 +3,8 @@
 #' @return A data.frame with pollen forecast data
 #' @export
 #' @name get_pollen_forecast
+#' @examples
+#' df_test <- get_pollen_forecast(latitude=46.94, longitude=7.94)
 
 get_pollen_forecast <- function(latitude, longitude) {
   url <- paste0(
@@ -12,16 +14,16 @@ get_pollen_forecast <- function(latitude, longitude) {
     "&location.longitude=", longitude,
     "&days=1"
   )
-  # 
+  #
   response <- httr2::request(url) |> httr2::req_perform()
   content <- httr2::resp_body_json(response)
-  
+
   if (is.null(content$dailyInfo[[1]]$pollenTypeInfo)) {
     return(data.frame())
   }
-  
+
   pollen_info <- content$dailyInfo[[1]]$pollenTypeInfo
-  
+
   df <- do.call(rbind, lapply(pollen_info, function(x) {
     data.frame(
       plant = x$displayName,
@@ -32,7 +34,7 @@ get_pollen_forecast <- function(latitude, longitude) {
       stringsAsFactors = FALSE
     )
   }))
-  
+
   return(df)
 }
 
